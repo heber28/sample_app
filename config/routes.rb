e8f2f9463f018b88293d1d8ada8af6abfffb382d
Heber Massami Nunomura <heber28@gmail.com>
2010-11-21 14:43:39 -0200

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,15 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+
+  # You can have the root of your site routed with map.root 
+  # -- just remember to delete public/index.html.
+  map.root :controller => 'pages', :action => 'home'
+  #map.home '/', :controller => 'pages', :action => 'home'
+
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.contact '/about', :controller => 'pages', :action => 'about'
+  map.contac '/help', :controller => 'pages', :action => 'help'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
