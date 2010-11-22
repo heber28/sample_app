3fdea9c87ad89a22cb4c96d1e556fdae2fb6c7d6
Heber Massami Nunomura <heber28@gmail.com>
2010-11-22 11:01:42 -0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -45,8 +45,9 @@ ActionController::Routing::Routes.draw do |map|
   #map.home '/', :controller => 'pages', :action => 'home'
 
   map.contact '/contact', :controller => 'pages', :action => 'contact'
-  map.contact '/about', :controller => 'pages', :action => 'about'
-  map.contac '/help', :controller => 'pages', :action => 'help'
+  map.about '/about', :controller => 'pages', :action => 'about'
+  map.help '/help', :controller => 'pages', :action => 'help'
+  map.signup '/signup', :controller => 'users', :action => 'new'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
