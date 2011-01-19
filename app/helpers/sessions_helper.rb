a97299b85e36ce4a300cec7ccbf532000775fb5f
Heber Massami Nunomura <heber28@gmail.com>
2011-01-19 16:50:56 -0200

    Done with sign in - with tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     user.remember_me!
-    cookies[:remember_toker] = { :value => user.remember_token,
+    cookies[:remember_token] = { :value => user.remember_token,
                                  :expires => 20.years.from_now.utc }
     self.current_user = user
   end
